0a17dd3384b271dce327dfae636b22833c203db0
MMartincorena <martinmartincorena@gmail.com>
2022-04-09 21:32:50 -0300

    Inicio y credenciales agregadas

---
+++ src/firebase/credenciales.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACtW5kS-95N11LV62N_qeeWJZ1QafRTPs",
+  authDomain: "login-register-firebase-49df1.firebaseapp.com",
+  projectId: "login-register-firebase-49df1",
+  storageBucket: "login-register-firebase-49df1.appspot.com",
+  messagingSenderId: "212736609373",
+  appId: "1:212736609373:web:c3338536ffd31ecb8e51b4"
+};
+
+const app = initializeApp(firebaseConfig);
